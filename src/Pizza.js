3cbe1cd71af2ea145eb655f0565ce9e27175ce9c
Jeovanni <jeovannildg@gmail.com>
2020-06-14 20:48:11 -0600

    added size select option to form

--- src/Pizza.js
+++ src/Pizza.js
@@ -3,8 +3,16 @@ import React from "react";
 const Pizza = () => {
     return (
         <div className="pizza-form">
-            <form>  
-
+            <form onSubmit={formSubmit}>  
+                <label htmlFor="name">
+                    Choice of Size
+                    <select id="pizza-size" name="sizelist">
+                        <option value="small">Small</option>
+                        <option value="medium">Small</option>
+                        <option value="large">Small</option>
+                        <option value="xlarge">Extra Large</option>
+                    </select>
+                </label>
             </form>
         </div>
     );
